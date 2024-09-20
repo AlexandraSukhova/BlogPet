23825cdf39991374af9a4773cdc03fe8baf699b6
Alexandra Sukhova <sukhova_alexandra@mail.ru>
2024-09-20 21:51:28 +0300

    fsd

--- BlogPet/vite.config.ts
+++ BlogPet/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })],
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
 })
